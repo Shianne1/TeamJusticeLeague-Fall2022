f36f30187aae19537f692052ede0dea12b67f182
JagdMarx <cmanapier@gmail.com>
2022-10-12 14:27:30 -0400

    added a short system.out to show that something was changed - Mark

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,12 @@
+
 public class Assignment {
+
+
+    // change made by JagdMarx in Intellij, added through gitHub
+       public static void main(String[] args){
+
+       System.out.println("This is a simple change");
+   }
+
+
 }
